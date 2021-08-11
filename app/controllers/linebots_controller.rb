c62dd0ec5a2dc735df78b60d6e968727d2b05cd2
maryoryo <liyomaxxde@gmail.com>
2021-08-11 14:56:06 +0000

    [linebot-fix]20210811

--- app/controllers/linebots_controller.rb
+++ app/controllers/linebots_controller.rb
@@ -1,52 +1,64 @@
 class LinebotsController < ApplicationController
   
-#   # app.rb
-# # require 'sinatra'
-# require 'line/bot'
+  
+  # require 'line/bot'  # gem 'line-bot-api'
 
-# def client
-#   @client ||= Line::Bot::Client.new { |config|
-#     config.channel_id = ENV["LINE_CHANNEL_ID"]
-#     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-#     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-#   }
-# end
+  # # callbackアクションのCSRFトークン認証を無効
+  # # protect_from_forgery :except => [:callback]
+  # protect_from_forgery with: :null_session
 
+  # def client
+  #   @client ||= Line::Bot::Client.new { |config|
+  #     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+  #     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+  #   }
+  # end
 
-# post '/callback' do
-#   body = request.body.read
+  # def callback
 
-#   signature = request.env['HTTP_X_LINE_SIGNATURE']
-#   unless client.validate_signature(body, signature)
-#     error 400 do 'Bad Request' end
-#   end
+  #   # Postモデルの中身をランダムで@postに格納する
+  #   @post=Post.offset( rand(Post.count) ).first
+  #   body = request.body.read
 
-#   events = client.parse_events_from(body)
-#   events.each do |event|
-#     case event
-#     when Line::Bot::Event::Message
-#       case event.type
-#       when Line::Bot::Event::MessageType::Text
-#         message = {
-#           type: 'text',
-#           text: event.message['text']
-#         }
-#         client.reply_message(event['replyToken'], message)
-#       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-#         response = client.get_message_content(event.message['id'])
-#         tf = Tempfile.open("content")
-#         tf.write(response.body)
-#       end
-#     end
-#   end
+  #   signature = request.env['HTTP_X_LINE_SIGNATURE']
+  #   unless client.validate_signature(body, signature)
+  #     head :bad_request
+  #   end
 
-#   # Don't forget to return a successful response
-#   "OK"
-#   # render json: user, status: 200
-#   # def response_success(class_name, action_name)
-#   #   render status: 200, json: { status: 200, message: "Success #{class_name.capitalize} #{action_name.capitalize}" }
-#   # end
-# end
+  #   events = client.parse_events_from(body)
+
+  #   events.each { |event|
+
+  #     # event.message['text']でLINEで送られてきた文書を取得
+  #     if event.message['text'].include?("好き")
+  #       response = "んほぉぉぉぉぉぉ！すきすきすきすきすきすきすきすきぃぃぃぃぃ"
+  #     elsif event.message["text"].include?("行ってきます")
+  #       response = "どこいくの？どこいくの？どこいくの？寂しい寂しい寂しい。。。"
+  #     elsif event.message['text'].include?("おはよう")
+  #       response = "おはよう。なんで今まで連絡くれなかったの？"
+  #     elsif event.message['text'].include?("みーくん")
+  #       response = "みーくん！？" * 50
+  #     else
+  #       response = @post.name
+  #     end
+  #     #if文でresponseに送るメッセージを格納
+
+  #     case event
+  #     when Line::Bot::Event::Message
+  #       case event.type
+  #       when Line::Bot::Event::MessageType::Text
+  #         message = {
+  #           type: 'text',
+  #           text: response
+  #         }
+  #         client.reply_message(event['replyToken'], message)
+  #       end
+  #     end
+  #   }
+
+  #   head :ok
+  # end
+  
 
 
   require 'line/bot'
@@ -67,9 +79,6 @@ class LinebotsController < ApplicationController
     @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-      # ローカルで動かすだけならベタ打ちでもOK
-      # config.channel_secret = "your channel secret"
-      # config.channel_token = "your channel token"
     }
   end
   
@@ -78,6 +87,19 @@ class LinebotsController < ApplicationController
     events = client.parse_events_from(body)
 
     events.each { |event|
+    
+      # if event.message['text'].include?("好き")
+      #   response = "んほぉぉぉぉぉぉ！すきすきすきすきすきすきすきすきぃぃぃぃぃ"
+      # elsif event.message["text"].include?("行ってきます")
+      #   response = "どこいくの？どこいくの？どこいくの？寂しい寂しい寂しい。。。"
+      # elsif event.message['text'].include?("おはよう")
+      #   response = "おはよう。なんで今まで連絡くれなかったの？"
+      # elsif event.message['text'].include?("みーくん")
+      #   response = "みーくん！？" * 50
+      # else
+      #   response = @post.name
+      # end
+    
       case event
       when Line::Bot::Event::Message
         case event.type
@@ -94,8 +116,7 @@ class LinebotsController < ApplicationController
         end
       end
     }
-    # "OK"
-    head :ok
+    "OK"
   end
   
 end
